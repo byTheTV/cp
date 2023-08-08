157395d89fdd029f7190d9249976a07b6fe55343
byTheTV <108927536+byTheTV@users.noreply.github.com>
2023-08-08 14:34:41 +0300

    Add files via upload

--- Tree(max).cpp
+++ Tree(max).cpp
@@ -1,9 +1,10 @@
 #include <vector>
 #include <iostream>
 #define ll long long
+#define pii pair<int, int>
 using namespace std;
 
-vector<int> tree;
+vector<pii> tree;
 vector<int> a;
 
 ll mod = 1e10;
@@ -12,9 +13,9 @@ void build(int v, int tl, int tr) {
 	if (tl >= tr)
 		return;
 	if (tl + 1 == tr) {
-		tree[v] = a[tl];
+		tree[v] = { a[tl], tl };
 		return;
-	}		
+	}
 	int tm = (tl + tr) / 2;
 	build(v * 2, tl, tm);
 	build(v * 2 + 1, tm, tr);
@@ -25,15 +26,15 @@ void update(int v, int tl, int tr, int pos, int new_elem) {
 	if (tl >= tr)
 		return;
 	if (tl + 1 == tr) {
-		tree[v] = new_elem;
+		tree[v] = { new_elem, pos };
 		return;
 	}
 	int tm = (tl + tr) / 2;
-//	cout << tm << " ";
-	if (tm >= pos)
+	if (tm >= pos) {
 		update(v * 2, tl, tm, pos, new_elem);
-	else
+	} else {
 		update(v * 2 + 1, tm, tr, pos, new_elem);
+	}
 	tree[v] = max(tree[v * 2], tree[v * 2 + 1]);
 }
 
@@ -41,14 +42,13 @@ int find(int v, int tl, int tr, int left, int right) {
 	if (tl >= tr || left >= right)
 		return -mod;
 	if (left <= tl && tr <= right) {
-		return tree[v];
+		return tree[v].first;
 	}
 	int tm = (tl + tr) / 2;
 	return max(find(v * 2, tl, tm, left, min(tm, right)), find(v * 2 + 1, tm, tr, max(tm, left), right));
 }
 
-int main()
-{
+int main() {
 	int n;
 	cin >> n;
 	a.resize(n);
@@ -56,26 +56,18 @@ int main()
 		cin >> a[i];
 	tree.resize(4 * n + 1);
 	build(1, 0, n);
-
-	//for (int i = 1; i <= 4 * n; i++)
-	//	cout << tree[i] << " ";
-	//return 1;
-
 	ll k;
 	cin >> k;
 	for (int i = 0; i < k; i++) {
 		int l, r;
 		cin >> l >> r;
-//		l--, r--;
-		cout << find(1, 0, n, l, r) << "\n";
+		cout << find(1, 0, n, l - 1, r)<< "\n";
 	}
-//	update(1, 0, n, 0, 100);
-//	cout << "\n";
 }
 /*
 5
-1 2 3 4 5
+5 2 3 4 2
 1
-2 3
+2 5
 
 */
